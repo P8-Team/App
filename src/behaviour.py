e901dc08fec92199299d3e03d0c771db6df81510
soenderby <jakob.soenderby@hotmail.com>
2022-03-10 14:23:33 +0100

    Basic classification based on number of frames

--- src/behaviour.py
+++ src/behaviour.py
@@ -2,6 +2,14 @@ from enum import Enum
 
 Label = Enum('Label', 'Ok Undesired')
 
+class Frame:
+  def __init__(self):
+        self.id = 0
+        self.rssi = []
+        self.packet_length = 0
+        self.mac_address = 0
+
+
 class Classifier:
-  def classify(self, packets: list):
-    return Label.Ok
+  def classify(self, frames: list):
+    return Label.Ok if len(frames) <= 2 else Label.Undesired
--- test/test_behaviour.py
+++ test/test_behaviour.py
@@ -1,4 +1,13 @@
-from src.behaviour import Classifier, Label
+from src.behaviour import Frame, Classifier, Label
 
-def test_classify():
-  assert Classifier().classify([]) == Label.Ok
+def test_classify_returns_undesired_if_given_more_than_2_frames():
+  frames = [Frame(), Frame(), Frame()]
+  assert Classifier().classify(frames) == Label.Undesired
+
+def test_classify_returns_ok_if_given_2_frames():
+  frames = [Frame(), Frame()]
+  assert Classifier().classify(frames) == Label.Ok
+
+def test_classify_returns_ok_if_given_less_than_2_frames():
+  frames = [Frame()]
+  assert Classifier().classify(frames) == Label.Ok
