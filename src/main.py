2ea3031c5a262585f26e6bbce64e9ed268676dba
Lau <ljosef18@student.aau.dk>
2022-03-10 14:55:00 +0100

    progress

--- src/main.py
+++ src/main.py
@@ -1,4 +1,21 @@
 
-# do tshark stuff
-import tshark
+# do pyshark stuff
+import json
+
+import pyshark
+
+if __name__ == '__main__':
+    # capture packets
+    # monitor mode
+    capture = pyshark.LiveCapture(interface='Wi-Fi', monitor_mode=True)
+    capture.set_debug()
+    for packet in capture:
+        print("=====================================================")
+        print("=====================================================")
+        print("=====================================================")
+        print(packet)
+        # serialize packets content to json
+
+        print(json.dumps(packet, indent=4))
+        break
 
