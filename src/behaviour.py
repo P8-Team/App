8eab02ba350a2dbfa97fb228c6fd1c3984f3535b
soenderby <jakob.soenderby@hotmail.com>
2022-03-10 14:45:27 +0100

    Added validation of arguments

--- src/behaviour.py
+++ src/behaviour.py
@@ -12,4 +12,11 @@ class Frame:
 
 class Classifier:
   def classify(self, frames: list):
+    # Check that arguments are arguments
+    if len(frames) == 0:
+      raise ValueError('classify must given non-empty list of frames')
+    if any(not isinstance(item, Frame) for item in frames):
+      raise ValueError('classify must be given list with elements of type Frame')
+
+    # Classify behaviour
     return Label.Ok if len(frames) <= 2 else Label.Undesired
--- test/test_behaviour.py
+++ test/test_behaviour.py
@@ -1,3 +1,4 @@
+import pytest
 from src.behaviour import Frame, Classifier, Label
 
 def test_classify_returns_undesired_if_given_more_than_2_frames():
@@ -11,3 +12,11 @@ def test_classify_returns_ok_if_given_2_frames():
 def test_classify_returns_ok_if_given_less_than_2_frames():
   frames = [Frame()]
   assert Classifier().classify(frames) == Label.Ok
+
+def test_classify_throws_exception_if_not_given_non_empty_list_of_frames():
+  with pytest.raises(TypeError):
+    Classifier().classify(2)
+  with pytest.raises(ValueError):
+    Classifier().classify([])
+  with pytest.raises(ValueError):
+    Classifier().classify([1, 'test', 3])
