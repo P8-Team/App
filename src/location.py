0dfe76cc8de229757395f89f94405494acc89b60
soenderby <jakob.soenderby@hotmail.com>
2022-03-17 12:42:30 +0100

    Fixed implementation of common_intersection
    Also added comments explaining what is going on.

--- src/location.py
+++ src/location.py
@@ -9,17 +9,21 @@ def common_intersection(circles):
   if len(circles) < 2:
     raise ValueError('common_intersection must be given at least two Circles')
 
+  # Find intersections of the first two circles
   intersections = set(compare(circles[0], circles[1]))
 
-  for a, b in itertools.combinations(circles[2:], 2):
-    intersections.update(set(compare(a, b)))
+  # Find intersections of all pairs of circles
+  # Take set intersection of the previous values and the new values
+  # This will ensure that the result set will only contain points where all the circles intersect
+  for a, b in itertools.combinations(circles, 2):
+    intersections.intersection(compare(a, b))
 
   result = list(intersections)
-
+  
   if len(result) > 0:
     return result[0]
   else:
     return None
 
 def compare(a, b):
-  return a.intersection(b)
+  return set(a.intersection(b))
