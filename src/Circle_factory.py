094b03d0274cf8c8804c614fec5afaa35dc802de
soenderby <jakob.soenderby@hotmail.com>
2022-03-18 14:11:04 +0100

    Added tests for type checking

--- src/Circle_factory.py
+++ src/Circle_factory.py
@@ -5,13 +5,13 @@ from src.utils import is_point
 
 def circle_factory(point: Point, radius: float):
     """
-
     :param point: Takes an input of the type sympy.Point
     :param radius: Takes a radius of the circle.
     :return: A circle.
     """
     if not is_point(point):
         raise TypeError("First argument must be a sympy.Point.")
+    # TODO: Create type checking function is_number in the utils module
     if not (isinstance(radius, float) or isinstance(radius, int)):
         raise TypeError("Second argument must be a number.")
     return Circle(point, radius)
@@ -20,10 +20,10 @@ def circle_factory(point: Point, radius: float):
 def circle_factory_lst(circles: list):
     """
     Takes a list of elements, and converts them into circles.
-    :param circles: Takes a list of arrays of the form [X,Y,Radius]
+    :param circles: Takes a list of lists of the form [X,Y,Radius]
     :return: List of circles
     """
     for elem in circles:
-        if len(elem) < 3:
-            raise TypeError("Array does not contain all values")
+        if len(elem) != 3:
+            raise ValueError("Elements must be lists containing three elements")
         circle_factory(Point(elem[0], elem[1]), elem[2])
--- test/test_circle_factory.py
+++ test/test_circle_factory.py
@@ -1,7 +1,7 @@
 import pytest
 from sympy import Point
 
-from src.Circle_factory import circle_factory
+from src.Circle_factory import circle_factory, circle_factory_lst
 from src.utils import is_circle
 
 
@@ -21,3 +21,12 @@ def test_circle_factory_returns_circle_gets_float_second_parameter():
 
 def test_circle_factory_returns_circle_gets_int_second_parameter():
     assert is_circle(circle_factory(Point(2, 2), 5))
+
+
+def test_circle_factory_lst_throws_exception_if_elements_are_incorrect():
+    with pytest.raises(ValueError):
+        circle_factory_lst([[1,2]])
+    with pytest.raises(ValueError):
+        circle_factory_lst([[1,2,3,4]])
+    with pytest.raises(ValueError):
+        circle_factory_lst([[1,2,3], [4,5,6], [7,8]])
