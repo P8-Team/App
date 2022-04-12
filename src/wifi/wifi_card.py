5fce73fa91ce747d5fd3be00a7f811c4d37463d6
Lau <ljosef18@student.aau.dk>
2022-04-12 13:34:27 +0200

    Refactor

--- src/wifi/wifi_card.py
+++ src/wifi/wifi_card.py
@@ -5,3 +5,6 @@ class WifiCard:
     def __init__(self, interface_name: str, location: [float, float]):
         self.interface_name = interface_name
         self.location = location
+
+    def __repr__(self) -> str:
+        return f"WifiCard(interface_name={self.interface_name}, location={self.location})"
