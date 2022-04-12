b66c9eec73a40e0737250f863d194a7df69dc244
soenderby <jakob.soenderby@hotmail.com>
2022-04-12 14:54:40 +0200

    Added tests for channel_hopper constructor

--- test/test_channel_hopper.py
+++ test/test_channel_hopper.py
@@ -1,3 +1,4 @@
+from distutils.util import change_root
 import pytest
 from src.channel_hopper import ChannelHopper
 import time
@@ -10,3 +11,28 @@ def test_channel_hopper_using_start():
     channel_hopper.start()
     time.sleep(5)
     channel_hopper.stop()
+
+def test_channel_hopper_sets_interfaces():
+    channel_hopper = ChannelHopper(['1','2','3'])
+
+    assert channel_hopper.interfaces == ['1','2','3']
+
+def test_channel_hopper_default_channels():
+    channel_hopper = ChannelHopper([''])
+
+    assert channel_hopper.channels == [1,2,3,4,5,6,7,8,9,10,11,12,13]
+
+def test_channel_hopper_uses_given_channels():
+    channel_hopper = ChannelHopper([''], channels = [2,4,6,8888])
+
+    assert channel_hopper.channels == [2,4,6,8888]
+
+def test_channel_hopper_default_sleep_time():
+    channel_hopper = ChannelHopper([''])
+
+    assert channel_hopper.sleep_time == 2
+
+def test_channel_hopper_uses_given_time_between_hops():
+    channel_hopper = ChannelHopper([''], time_between_hops_sec= 87.2)
+
+    assert channel_hopper.sleep_time == 87.2
