8d370a93ab1bcfe1914f6ad758a3341bc3ba8627
Mikael Vind Mikkelsen <mimikk21@student.aau.dk>
2022-04-25 10:24:27 +0200

    Small Changes

--- test/test_device_lookup.py
+++ test/test_device_lookup.py
@@ -12,11 +12,12 @@ def test_data_is_loaded():
 
 def test_find_selected_data():
     dl = DeviceLookup()
-    test_data = pd.read_csv(r'..\Data\test_data.csv', index_col=['Labels'])
+    test_data = pd.read_csv(r'..\test\test_data\device_data.csv', index_col=['Labels'])
     dl.lookup_data = pd.DataFrame(test_data)
 
     assert dl.all_mac_addresses() == ['42:R2:D2:C3:PO:42', 'A1:B2:C3:D4:E5:F6']
-    assert dl.lookup_device_by_label('LargeOrc') == [-20, '42:R2:D2:C3:PO:42', 'Limited Edition Thrall Camera']
-    assert dl.lookup_device_by_label('Wink') == [-10, 'A1:B2:C3:D4:E5:F6', 'Totally Not a Cam Camera']
-
+    assert dl.get_device_info_by_label('LargeOrc') == [-20, '42:R2:D2:C3:PO:42', 'Limited Edition Thrall Camera']
+    assert dl.get_transmission_power_by_label('LargeOrc') == -20
+    assert dl.get_device_info_by_label('Wink') == [-10, 'A1:B2:C3:D4:E5:F6', 'Totally Not a Cam Camera']
+    assert dl.get_transmission_power_by_label('Wink') == -10
 
