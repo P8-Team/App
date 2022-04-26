002647a00c6b40646a847472d2f09e73c1f0a105
Mikael Vind Mikkelsen <mimikk21@student.aau.dk>
2022-04-26 15:08:51 +0200

    WIP1

--- test/test_classifier.py
+++ test/test_classifier.py
@@ -1,15 +1,16 @@
-import pytest
+import pandas as pd
 from src.classifier import Classifier
-
 from src.wifi.wifi_card import WifiCard
 from src.wifi.wifi_frame import WifiFrame
+from test.utils.wifi_frame_factory import frame_factory
 from test.utils.wifi_test_utils import Frame, Layer
 from sympy import Point2D
-import pandas as pd
+
 
 def test_get_file_paths_returns_list_of_strings():
     classifier = Classifier(1)
     assert all(isinstance(x, str) for x in classifier.get_file_paths()) == True
+
 
 def test_preprocess_data():
     frame = Frame("340", "1647417907.513663000", {
@@ -36,3 +37,40 @@ def test_preprocess_data():
 
     assert result_labels == "test"
     assert len(result_df.columns) == 7
+
+
+def test_classify():
+    cl = Classifier(1)
+    frames = [frame_factory(1), frame_factory(2), frame_factory(2), frame_factory(2), frame_factory(3),
+              frame_factory(3), frame_factory(6)]
+    frames_db = pd.DataFrame
+    for frame in frames:
+        frames_db.append(frame.to_dataframe())
+    assert cl.classify(frames) is not None
+
+
+def test_classifier_has_labels():
+    cl = Classifier(1)
+    frames = [frame_factory(1), frame_factory(2), frame_factory(2), frame_factory(2), frame_factory(3),
+              frame_factory(3), frame_factory(6)]
+    frames_db = pd.DataFrame
+    for frame in frames:
+        frames_db.append(frame.to_dataframe())
+    labels = pd.DataFrame.from_dict({'Address': ["00:00:00:00:00:01"], 'Label': ['test']})
+
+    assert cl.labels_in_model() is list
+    assert len(cl.labels_in_model()) > 0
+    assert cl.labels_in_model()[0] is 'test'
+
+
+def test_classify_returns_label():
+    cl = Classifier(1)
+    #cl.train find alternativ
+    possible_label_list = cl.labels_in_model()
+    assert cl.classify_interval_label() in possible_label_list
+
+
+def test_classify_return_confidence():
+    cl = Classifier(1)
+    #cl.train find alternativ
+    assert 0 <= cl.classify_interval_confidence() <= 1
