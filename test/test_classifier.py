ccd810144b31ac3d3f900ecc2d56a55c7b79387b
soenderby <jakob.soenderby@hotmail.com>
2022-04-26 12:20:59 +0200

    Added some (flimsy) tests

---
+++ test/test_classifier.py
@@ -0,0 +1,39 @@
+from msilib.schema import Class
+import pytest
+from src.classifier import Classifier
+
+from src.wifi.wifi_card import WifiCard
+from src.wifi.wifi_frame import WifiFrame
+from test.utils.wifi_test_utils import Frame, Layer
+from sympy import Point2D
+import pandas as pd
+
+def test_get_file_paths_returns_list_of_strings():
+    classifier = Classifier(1)
+    assert all(isinstance(x, str) for x in classifier.get_file_paths()) == True
+
+def test_preprocess_data():
+    frame = Frame("340", "1647417907.513663000", {
+        'wlan': Layer({
+            'fcs': '0x1234',
+            'wlan.fc.type': '0',
+            'wlan.fc.subtype': '4',
+            'wlan.ra_resolved': '00:0c:29:b7:d9:b0',
+            'wlan.ta_resolved': '00:0c:29:b7:d9:b1',
+        }),
+        'wlan_radio': Layer({
+            'wlan_radio.signal_dbm': '-62',
+            'wlan_radio.data_rate': '54',
+            'wlan_radio.timestamp': '1567757308'
+        })
+    })
+
+    wifi_frame = WifiFrame.from_frame(frame, WifiCard("wlan0", Point2D(0, 0)))
+    labels = pd.DataFrame.from_dict({'Address': ["00:0c:29:b7:d9:b1"], 'Label': ['test']})
+
+    classifier = Classifier(1)
+
+    result_df, result_labels = classifier.preprocess_data(wifi_frame.to_dataframe(), labels)
+
+    assert result_labels == "test"
+    assert len(result_df.columns) == 7
