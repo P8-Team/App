4ab26de7acb4c650369c6498df97d44fdd8967a4
soenderby <jakob.soenderby@hotmail.com>
2022-04-26 11:10:14 +0200

    Fixed tests
    Turns out the CI runner does not have tshark. So I refactored so the tests do not use tshark.

--- src/multiprocess_wifi_listener.py
+++ src/multiprocess_wifi_listener.py
@@ -13,6 +13,7 @@ from sympy import Point2D
 from src.wifi.wifi_card import WifiCard
 from src.wifi.wifi_frame import WifiFrame
 
+
 def wifi_listener(wifi_card: WifiCard, queue: Queue) -> None:
     """
         Starts a listener on a given Wi-Fi interface name
@@ -51,8 +52,8 @@ def multiprocess_wifi_listener(wifi_card_list: list[WifiCard]) -> Iterator[WifiF
 def frames_from_file_with_caching(file_path: str) -> Generator[WifiFrame, None, None]:
     cache_folder = "Data/cache"
     if not exists(f'{cache_folder}/{sha1(file_path.encode("utf-8")).hexdigest()}.json'):
-        print('No cached dataframe found. loading frames from file')
-        frames = frames_from_file(file_path)
+        print(f'No cached dataframe found for {file_path}. loading frames from file')
+        frames = map_to_frames(load_file(file_path), WifiCard("file", Point2D(0, 0)))
         dfs = list()
         for item in frames:
             dfs.append(item.to_dataframe())
@@ -61,48 +62,22 @@ def frames_from_file_with_caching(file_path: str) -> Generator[WifiFrame, None, 
         cache_dataframe(cache_folder, file_path, df)
         return df
     else:
-        print('Using cached dataframe')
+        print(f'Using cached dataframe for {file_path}')
         return load_cached_dataframe(cache_folder, file_path)
+
+def load_file(file_path: str) -> Generator[WifiFrame, None, None]:
+    if not exists(file_path):
+        raise FileNotFoundError
+    file = pyshark.FileCapture(file_path)
+    result = list(file)
+    file.close()
+    return result
+
+def map_to_frames(iterator, wifi_card):
+    return list(map(lambda x: WifiFrame.from_frame(x, wifi_card), iterator))
 
 def cache_dataframe(cache_folder: str, name: str, df: DataFrame) -> None:
     df.to_json(f"{cache_folder}/{sha1(name.encode('utf-8')).hexdigest()}.json", orient='records', default_handler=str)
 
 def load_cached_dataframe(cache_folder: str, name:str) -> DataFrame:
     return pd.read_json(f"{cache_folder}/{sha1(name.encode('utf-8')).hexdigest()}.json", orient='records')
-
-def frames_from_file(file_path: str) -> Generator[WifiFrame, None, None]:
-    """
-        Creates a generator that yields WifiFrames created from the given file
-    :param file_name: Path to the file
-    :return: Generator of WifiFrames
-    """
-
-    if not exists(file_path):
-        raise FileNotFoundError
-    print("Reading frames from {}".format(file_path))
-    wifi_card = WifiCard("file", Point2D(0, 0))
-
-    file = pyshark.FileCapture(file_path)
-    frames = list(file)
-    print(f'file contains {len(frames)} frames')
-    file.close()
-    for frame in frames:
-        yield WifiFrame.from_frame(frame, wifi_card)
-
-def wifi_listener_from_file(file_name: str, queue: Queue):
-    """
-        Starts a listener on a given file.
-    :param file_name: File path to the file
-    :param queue:
-    :return:
-    """
-
-    if not exists(file_name):
-        raise FileNotFoundError
-    print("Starting Listener on {}".format(file_name))
-    wifi_card = WifiCard("file", Point2D(0, 0))
-
-    file = pyshark.FileCapture(file_name)
-    for frame in file:
-        queue.put(WifiFrame.from_frame(frame, wifi_card))
-    file.close()
--- test/test_multiprocess_wifi_listener.py
+++ test/test_multiprocess_wifi_listener.py
@@ -4,22 +4,48 @@ import json
 from os.path import exists
 import os
 from hashlib import sha1
-
-
 import pytest
+from src.multiprocess_wifi_listener import cache_dataframe, load_cached_dataframe, map_to_frames
+from test.utils.wifi_test_utils import Frame, Layer
+from src.wifi.wifi_frame import WifiFrame
+from src.wifi.wifi_card import WifiCard
+from sympy import Point2D
 
-from src.multiprocess_wifi_listener import frames_from_file, cache_dataframe, load_cached_dataframe
+frame1 = Frame("340", "1647417907.513663000", {
+    'wlan': Layer({
+        'fcs': '0x1234',
+        'wlan.fc.type': '0',
+        'wlan.fc.subtype': '4',
+        'wlan.ra_resolved': '00:0c:29:b7:d9:b0',
+        'wlan.ta_resolved': '00:0c:29:b7:d9:b1',
+    }),
+    'wlan_radio': Layer({
+        'wlan_radio.signal_dbm': '-62',
+        'wlan_radio.data_rate': '54',
+        'wlan_radio.timestamp': '1567757308'
+    })
+})
 
-def test_wifi_frames_from_file_given_nonexistent_file():
-    filepath = "NOT A VALID FILE PATH"
-    with pytest.raises(FileNotFoundError):
-        next(frames_from_file(filepath))
+frame2 = Frame("111", "1647417907.513663000", {
+    'wlan': Layer({
+        'fcs': '0x1235',
+        'wlan.fc.type': '0',
+        'wlan.fc.subtype': '4',
+        'wlan.ra_resolved': '00:0c:29:b7:d9:b1',
+        'wlan.ta_resolved': '7c:49:eb:40:22:2f',
+    }),
+    'wlan_radio': Layer({
+        'wlan_radio.signal_dbm': '-62',
+        'wlan_radio.data_rate': '54',
+        'wlan_radio.timestamp': '1567757312'
+    })
+})
 
-def test_wifi_frames_from_file_given_existing_file():
-    filepath = "test/test_data/dump.pcapng"
-    gen = frames_from_file(filepath)
-    first_element = next(gen)
-    assert first_element.frame_control_information.transmitter_address == "7c:49:eb:40:22:2f"
+def test_map_to_frames():
+    wifi_card = WifiCard("test", Point2D(0, 0))
+    expected = [WifiFrame.from_frame(frame1, wifi_card), WifiFrame.from_frame(frame2, wifi_card)]
+    result = map_to_frames([frame1, frame2], wifi_card)
+    assert result == expected
 
 def test_cache_dataframe():
     data = {'col_1': [3, 2, 1, 0], 'col_2': ['a', 'b', 'c', 'd']}
