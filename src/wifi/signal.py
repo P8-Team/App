cc37e0c386cfb2b81e3e2b89aec2788f8e4aa643
soenderby <jakob.soenderby@hotmail.com>
2022-05-04 15:36:13 +0200

    Added functionality for calculating timedelta

--- src/wifi/signal.py
+++ src/wifi/signal.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import pandas as pd
 from sympy import Point2D
 
@@ -13,8 +15,16 @@ class Signal:
         self.signal_strength = signal_strength
         self.sniff_timestamp = sniff_timestamp
         self.variance = variance
+        self.timestamp_delta = None
 
     def to_dataframe(self):
         return pd.DataFrame(data={
             'signal_strength': [self.signal_strength], 'sniff_timestamp': [self.sniff_timestamp]
         })
+
+    def set_timestamp_delta_from_other_signal(self, other: Signal):
+        self.timestamp_delta = self.sniff_timestamp_delta(other, self)
+
+    @staticmethod
+    def sniff_timestamp_delta(old_signal: Signal, new_signal: Signal):
+        return new_signal.sniff_timestamp - old_signal.sniff_timestamp
