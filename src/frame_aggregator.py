8e2f135d25882a9999dda7ec1cf98fc81bf35692
Ane <anesoejoe@gmail.com>
2022-05-18 12:53:46 +0200

    Removed TODO

--- src/frame_aggregator.py
+++ src/frame_aggregator.py
@@ -13,7 +13,7 @@ def frame_aggregator(generator, threshold=None, max_age_seconds=None, max_buffer
     :param max_buffer_size: max number of frames in buffer
     :return: generator that yields combined frames
     """
-    # TODO: Maybe add these to config?
+    
     if threshold is None:
         threshold = 3
     if max_age_seconds is None:
