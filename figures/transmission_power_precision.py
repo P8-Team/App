8eb0a293ace153e20c90f87c9960cb886d512904
Ane <anesoejoe@gmail.com>
2022-05-24 19:15:27 +0200

    Test stuff

--- figures/transmission_power_precision.py
+++ figures/transmission_power_precision.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from sympy import Point2D
 
 from src.data_generators.location_data_generator import LocationGenerator
@@ -72,17 +74,20 @@ def average_error():
 
 
 def error_at_distance(distance):
-    wifi_frame_generator = LocationGenerator([Point2D([0, 4.33]), Point2D([5, -4.33]), Point2D([-5, -4.33])],
+    wifi_frame_generator = LocationGenerator([Point2D([0, -1]), Point2D([1, 0]), Point2D([0, 1])],
                                              rounding=False)
+
     path_loss_exponent = 4
     actual_point = Point2D([0, distance])
 
     tx_power_max = 100
     tx_power_min = -100
-    tx_power_actual = [-100, 100]
+    tx_power_actual = [-23, -17, -6]
     tx_power_step = 0.5
 
     for tx_power in tx_power_actual:
+        error_points = []
+        points = []
         wifi_frame = wifi_frame_generator.make_wifi_element(Point2D([0, distance]),
                                                             transmission_power_dbm=tx_power)
         device = Device("0", [wifi_frame])
@@ -99,15 +104,53 @@ def error_at_distance(distance):
         while tx_power_current <= tx_power_max:
             calculate_position(device, path_loss_exponent, tx_power_current, 0)
 
+            points.append([device.position[0], device.position[1]])
+
             error = float(Point2D(device.position).distance(actual_point))
+
+            error_points.append([tx_power_current, error])
 
             print(f"{tx_power}\t{tx_power_current}\t{error}")
 
             tx_power_current = tx_power_current + tx_power_step
 
+        # draw_path(error_points)
+        draw_points(points, [0, distance])
+
+
+def draw_points(points, actual):
+    fig, ax = plt.subplots()
+
+    ax.set_xlim(-5, 5)
+    ax.set_ylim(-5, 5)
+
+    data = np.array(points)
+    plt.scatter(data[:, 0], data[:, 1])
+    actual_point = np.array([actual])
+    plt.scatter(actual_point[:, 0], actual_point[:, 1])
+    plt.show()
+
+
+def draw_path(points):
+    # codes = [Path.MOVETO]
+    #
+    # for i in range(len(points) - 1):
+    #     codes.append(Path.LINETO)
+
+    # path = Path(points, codes)
+
+    # fig, axs = plt.subplots(1, 2)
+    fig, ax = plt.subplots()
+
+    ax.set_xlim(-100, 100)
+    ax.set_ylim(0, 40)
+
+    data = np.array(points)
+    plt.plot(data[:, 0], data[:, 1])
+
 
 if __name__ == '__main__':
     # average_error()
 
-
-    error_at_distance(10)
+    error_at_distance(5)
+    plt.show()
